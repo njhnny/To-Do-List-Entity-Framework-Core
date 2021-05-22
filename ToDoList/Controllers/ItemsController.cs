b2fe930316fca2144e12fe4e0543894fe0b0c3f0
Nick Hennessy <njhnny@gmail.com>
2021-05-21 23:38:39 -0700

    update views with ViewBag

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -23,6 +24,7 @@ namespace ToDoList.Controllers
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
@@ -42,6 +44,7 @@ namespace ToDoList.Controllers
     public ActionResult Edit(int id)
     {
     var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
     return View(thisItem);
     }
 
